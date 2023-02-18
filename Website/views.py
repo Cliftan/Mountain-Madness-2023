bcd303f00cc4a3a04092ecd9478c767bd6f4ae9c
Cliftan <clifton3833@gmail.com>
2023-02-18 15:58:11 -0800

    Setup

--- Website/views.py
+++ Website/views.py
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 def home():
     if request.method == "POST":
         search_term = request.form.get("search_term")
-    return "<h1> Hi </h1>"
+    return render_template("Search.html")
