b47ae0f0b9fc83130afaa2e5f65ecb1524babb08
Leo Zhi <leozhi6869@gmail.com>
2023-02-18 12:28:14 -0800

    api working!

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 # terminal pip3 install requests
 import requests
 
+#endpoint url, delineates what data we are interacting with
 url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 
 querystring = {"term":"wat"}
@@ -9,7 +10,15 @@ headers = {
 	"X-RapidAPI-Key": "921e591a9emsh1d05878404e0f88p13def1jsna7e4f4ef1e76",
 	"X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
 }
+def define(term):
+    querystring = {"term": term}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.json()
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+term = input("What term would you like to define? ")
+definition = define(term)
 
-print(response.text)
+if definition["list"]:
+    print(f"Definition of {term}: {definition['list'][0]['definition']}")
+else:
+    print(f"No definition found for {term}")
