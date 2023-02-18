a713c6a3b4e5eea62375d4c711a208e4d02d1e5d
Max Liu <024mliu@gmail.com>
2023-02-18 13:25:43 -0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,20 +2,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.urbandictionary.com/")
+def scraper(searchTerm):
+    response = requests.get("https://www.urbandictionary.com/"+searchTerm)
 
-soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(response.content, "html.parser")
 
-for i in soup.findAll('br'):
-    i.replace_with('\n')
+    for i in soup.findAll('br'):
+        i.replace_with('\n')
 
-results = soup.find(id = "ud-root")
-definition = results.find("div", class_ = "break-words meaning mb-4")
-if definition == None:
-    print("no definition found for this term")
-    exit()
+    results = soup.find(id = "ud-root")
+    definition = results.find("div", class_ = "break-words meaning mb-4")
+    definition = definition.text
+    if definition == None:
+        print("no definition found for this term")
+        exit()
 
-print(definition.text)
+    example = results.find("div", class_ = "break-words example italic mb-4")
+    example = example.text
+    
+    finalResult = (definition + "\n" + example)
+    return finalResult
 
-example = results.find("div", class_ = "break-words example italic mb-4")
-print(example.text)
+
