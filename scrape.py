d2a75474d9c26a9ff0ba80970d5c1f84ff7dd67f
Max Liu <024mliu@gmail.com>
2023-02-18 13:14:49 -0800

    Update scrape.py
    added case where word does not exist

--- scrape.py
+++ scrape.py
@@ -2,35 +2,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.urbandictionary.com/rizz")
+response = requests.get("https://www.urbandictionary.com/")
 
 soup = BeautifulSoup(response.content, "html.parser")
 
 for i in soup.findAll('br'):
     i.replace_with('\n')
-    
+
 results = soup.find(id = "ud-root")
 definition = results.find("div", class_ = "break-words meaning mb-4")
+if definition == None:
+    print("no definition found for this term")
+    exit()
+
 print(definition.text)
 
 example = results.find("div", class_ = "break-words example italic mb-4")
 print(example.text)
-
-
-
-
-# url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
-
-
-
-
-# querystring = {"term":"wat"}
-
-# headers = {
-# 	"X-RapidAPI-Key": "921e591a9emsh1d05878404e0f88p13def1jsna7e4f4ef1e76",
-# 	"X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
-# }
-
-# response = requests.request("GET", url, headers=headers, params=querystring)
-
-# print(response.text)
