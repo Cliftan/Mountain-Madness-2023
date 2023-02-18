211a0a2c60fe955a3b20862c9b8c7f8620ca893f
Leo Zhi <leozhi6869@gmail.com>
2023-02-18 13:05:28 -0800

    flask install push

--- api.py
+++ api.py
@@ -1,13 +1,12 @@
 # terminal pip3 install requests
 import requests
 
-#endpoint url for urban dictionary
+#endpoint url for urban dictionary api
 url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 
-
+#default query params for api key and host name
 querystring = {"term":"wat"}
 
-#default query params for api key and host name
 headers = {
 	"X-RapidAPI-Key": "921e591a9emsh1d05878404e0f88p13def1jsna7e4f4ef1e76",
 	"X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
@@ -33,3 +32,4 @@ if definition["list"]:
     print(f"Definition of {term}: {definition['list'][0]['definition']}")
 else:
     print(f"No definition found for {term}")
+
