2c0b99b82e2448eb818db4b66fb465f3fc172db6
Cliftan <clifton3833@gmail.com>
2023-02-18 23:40:22 -0800

    Final?

--- Website/views.py
+++ Website/views.py
@@ -3,14 +3,24 @@ from flask import Flask, redirect
 from scrape import scraper
 
 views = Blueprint('views', __name__)
+term = ""
+definition = ""
+example = ""
 
-
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/', methods=['POST','GET'])
 def home():
     if request.method == 'POST':
         search_term = request.form['q']
-        print(search_term)
-        return search_term, redirect('/')
+        global term
+        global definition
+        global example
+        term = search_term
+        list_scrape = scraper(term)
+        definition = list_scrape[0]
+        example = list_scrape[1]
+        return redirect('/result')
+    else:
+        print("WHAT THE FUCK " + request.method)
     return render_template("Search.html")
 
 @views.route('/error', methods=['GET', 'POST'])
@@ -19,4 +29,7 @@ def error():
 
 @views.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template("definition.html")
+    global term
+    global definition
+    global example
+    return render_template("definition.html", search=term, definition=definition, example=example)
