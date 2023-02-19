544e4aaa864cd53bbd12ee3e85d3e8a9f1bec1b8
Cliftan <clifton3833@gmail.com>
2023-02-18 17:38:58 -0800

    f

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from .views import views
+from .views import views, error
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'temporarypass'
 
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(error, url_prefix="/")
 
     return app
--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
+error = Blueprint("error", __name__)
 
 @views.route('/')
 def home():
     return render_template("Search.html")
+
+@error.route('/error')
+def home():
+    return render_template("error.html")
